e1137d1616e3e185c08a4589dd561be41a7ecb34
juan pico <juan.pico-m@mail.escuelaing.edu.co>
2022-07-19 10:05:09 -0500

    first commit

---
+++ src/test/java/co/edu/escuelaing/test/primer/live/HttpRequestTest.java
@@ -0,0 +1,28 @@
+package co.edu.escuelaing.test.primer.live;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+//correrlo en cualquier puerto abierto
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    // Para peticiones rest
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello, World");
+    }
+}
---
+++ src/test/java/co/edu/escuelaing/test/primer/live/TestingWebApplicationTests.java
@@ -0,0 +1,17 @@
+package co.edu.escuelaing.test.primer.live;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+      
+    }
+
+}
